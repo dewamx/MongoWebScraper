ea5fdc4ca0fcaaddd3ecea6382616e6212a1a596
raxemmohamed <raxemmohamed@gmail.com>
2019-08-21 11:55:26 -0500

    commit

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var router = require("express").Router();
+var apiRoutes = require("./api");
+var htmlRoutes = require("./html");
+
+router.use("/api", apiRoutes);
+router.use("/", htmlRoutes);
+
+module.exports = router;
